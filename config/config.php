911337050a62607d13c2cebd1bd0aff9c3a3cf60
jota <jpbl.bastos@gmail.com>
2014-08-16 11:27:10 -0300

    Arquivo de configurações 1.0.0

---
+++ config/config.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * 
+ * Este arquivo é parte do projeto sistema de votação - O verdadeiro sistema de pesquisa eleitoral
+ *
+ * Este programa é um software livre: você pode redistribuir e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU (GPL)como é publicada pela Fundação
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior
+ * e/ou 
+ * sob os termos da Licença Pública Geral Menor GNU (LGPL) como é publicada pela Fundação
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior.
+ *  
+ * Você deve ter recebido uma cópia da Licença Publica GNU e da 
+ * Licença Pública Geral Menor GNU (LGPL) junto com este programa.
+ * Caso contrário consulte <http://www.fsfla.org/svnwiki/trad/GPLv3> ou
+ * <http://www.fsfla.org/svnwiki/trad/LGPLv3>. 
+ *
+ * @package   sv
+ * @name      config.php
+ * @version   1.0.0
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
+ * @copyright 2014 Copyleft Porto Idéias 
+ * @author    Joao Paulo Bastos L. <jpbl.bastos at gmail dot com>
+ * @date      15-Ago-2014
+ *
+ */
+
+ /**
+  * 
+  * Configurações de parametros acesso ao banco de dados
+  * 
+  * @package sv
+  * @version 1.0.0
+  * @author  joao paulo bastos <jpbl.bastos at gmail dot com>
+  * 
+  */
+ 
+//Nome do Usuario
+$nomeUser="root";
+
+//Senha do usuario
+$passWd="129004";
+
+//Nome da Base de Dados
+$nomeDb="eleicao";
+
+//Host da Conexao
+$hostCon="localhost";
+
+?>
